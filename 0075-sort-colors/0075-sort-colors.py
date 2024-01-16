dc3f693129adc3e8a3da30c983773cd4d9c69ff3
Dhruv Kotwani <149938362+Luffy610@users.noreply.github.com>
2024-01-17 00:00:00 +0530

    Time: 36 ms (80.21%), Space: 17.3 MB (16.67%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        prefix = Counter(nums)
+        print(prefix)
+        for i in range(0,prefix[0]):
+            nums[i] = 0
+        prefix[1] += prefix[0]
+        for i in range(prefix[0],prefix[1]):
+            nums[i] = 1
+        prefix[2] += prefix[1]
+        for i in range(prefix[1], prefix[2]):
+            nums[i] = 2
