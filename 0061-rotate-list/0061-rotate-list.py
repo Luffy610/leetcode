46081c87230649cd2cc5afaaee3dd8e0627ba9b9
Dhruv Kotwani <149938362+Luffy610@users.noreply.github.com>
2024-03-18 22:55:27 +0530

    Time: 40 ms (45.33%), Space: 16.6 MB (42.91%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -1,35 +1,27 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+#Optimal Approach T.C: O(N) S.C.: O(1)
+
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if head is None:
-            return head
-        length = 1
-        temp = head
-        while temp.next:
-            temp = temp.next
-            length += 1
-            last = temp
-        if length < k:
-            index = length - k % length 
-        else:
-            index = length - k
-        temp_index = 0
-        if index == 0:
-            return head
-        temp = head
-        prev = None
-        while temp:
-            if temp_index == index:
-                prev.next = None
-                temp_head = head
-                head = temp
-                last.next = temp_head
-                break
-            prev = temp
-            temp_index += 1
-            temp = temp.next
-        return head
+            return None
+
+        curr = head
+        size = 1
+
+        # calculating length and making a circular loop
+        while curr.next is not None:
+            curr = curr.next
+            size += 1
+
+        curr.next = head
+
+        # cut from the rotating point
+        i = size - (k % size)
+
+        while i > 1:
+            head = head.next
+            i -= 1
+
+        curr = head.next
+        head.next = None
+        return curr
