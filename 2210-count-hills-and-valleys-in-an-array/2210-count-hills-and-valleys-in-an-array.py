ae0371b05aecaf54f443f27b8192aad0e1d13fb7
Dhruv Kotwani <149938362+Luffy610@users.noreply.github.com>
2024-05-30 00:55:09 +0530

    Time: 43 ms (41.98%), Space: 16.4 MB (93.28%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.py
@@ -1,34 +1,25 @@
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
-        i = 1
-        res = 0
-        previous = None
-        while i <= len(nums)-1:
-            left = i
-            right = i
-            while left >= 0:
-                if nums[left] != nums[i]:
-                    break
-                left = left - 1
-            else:
-                i += 1
-                continue
-            while right < len(nums):
-                if nums[right] != nums[i]:
-                    break
-                right += 1
-            else:
-                i += 1
-                continue
-            if nums[left] > nums[i] and nums[right] > nums[i] and previous != 'hill':
-                previous = 'hill'
-                res += 1
-            elif nums[left] < nums[i] and nums[right] < nums[i] and previous != 'valley':
-                previous = 'valley'
-                res += 1
-            i += 1
-        return res
+        result = 0
+        for i in range(1, len(nums)-1):
+            # edge case, the same velly!
+            # adjacent indexes should be considered as one velly or hill
+            # to handle this, we can check if next value is the same as current
+            # for example in [4,1,1,6]
+            # [4, 1(we are here), 1(detected next value), 6]
+            # replace the first 1 by 4 (4 is a previous value)
+            # as result we get: [4,4,1,6]
+            # so thet at the next iteration the second 1 will be detected as a valley
+            # [4,4,1(we are here),6] 
+            # 4 > 1 < 6
+            if nums[i] == nums[i+1]:
+                nums[i] = nums[i-1]
+            elif nums[i-1] > nums[i] < nums[i+1] or nums[i-1] < nums[i] > nums[i+1]:
+                result += 1
+        return result
+            
+
             
 
 
-        
+            
